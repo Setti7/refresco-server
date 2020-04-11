13ca44eb2df321fb06926c48964391d92e405be5
André Setti <ansetti7@gmail.com>
2020-04-11 18:13:13 -0300

    add graphql support

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,13 @@
-Parse.Cloud.define('hello', function(req, res) {
-  return 'Hi';
-});
+Parse.Cloud.define('hello', function (req, res) {
+  return 'Hi'
+})
+
+Parse.Cloud.define('getStoreGallons', async (req, res) => {
+  var store = new Parse.Object('Store')
+  store.id = req.params.storeId
+
+  return await store.relation('gallons').query().find()
+})
+
+// Parse.Cloud.define('initOrder', async (req, res) => {
+// })
--- index.js
+++ index.js
@@ -1,71 +1,55 @@
-// Example express application adding the parse-server module to expose Parse
-// compatible API routes.
-
-var express = require('express');
-var ParseServer = require('parse-server').ParseServer;
-var ParseDashboard = require('parse-dashboard');
+const express = require('express');
+const { default: ParseServer, ParseGraphQLServer } = require('parse-server');
+const ParseDashboard = require('parse-dashboard');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
+const app = express();
+
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
-var api = new ParseServer({
+const parseServer = new ParseServer({
   databaseURI: databaseUri,
   cloud: process.env.CLOUD_CODE_MAIN,
   appId: process.env.APP_ID,
-  masterKey: process.env.MASTER_KEY, //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL + process.env.PARSE_MOUNT,  // Don't forget to change to https if needed
-  liveQuery: {
-    // classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  masterKey: process.env.MASTER_KEY,
+  serverURL: process.env.SERVER_URL + '/parse',
 });
 
-// var api = new ParseServer({
-//   databaseURI: 'mongodb://localhost:27017/parse',
-//   cloud: __dirname + '/cloud/main.js',
-//   appId: '9UBUIZ0VeTdGe6YfwEg7KBbL8LSoM8ONAMQyLKzw',
-//   masterKey: 'UIBXVLMy8cFCE1bqxUW0Bh5pPWC5FejtH4BDga1j', //Add your master key here. Keep it secret!
-//   serverURL: 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-//   liveQuery: {
-//     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-//   }
-// });
-// Client-keys like the javascript key or the .NET key are not necessary with parse-server
-// If you wish you require them, you can set them as options in the initialization above:
-// javascriptKey, restAPIKey, dotNetKey, clientKey
+const parseGraphQLServer = new ParseGraphQLServer(
+  parseServer,
+  {
+    graphQLPath: '/graphql',
+    playgroundPath: '/playground'
+  }
+);
 
-var app = express();
-
-var dashboard = new ParseDashboard({
-  "apps": [
+const parseDashboard = new ParseDashboard({
+  apps: [
     {
-      "serverURL":  process.env.SERVER_URL + process.env.PARSE_MOUNT,
-      "appId": process.env.APP_ID,
-      "masterKey": process.env.MASTER_KEY,
-      "appName": "Refresco"
+      serverURL: process.env.SERVER_URL + '/parse',
+      appId: process.env.APP_ID,
+      masterKey: process.env.MASTER_KEY,
+      appName: 'Refresco',
+      graphQLServerURL: process.env.SERVER_URL + '/graphql'
     }
   ]
 });
 
-// Serve Parse Dashboard /dashboard
-app.use('/dashboard', dashboard);
 
-// Serve the Parse API at /parse
-var mountPath = process.env.PARSE_MOUNT ;
-app.use(mountPath, api);
-
+parseGraphQLServer.applyGraphQL(app); // Mounts the GraphQL API
+parseGraphQLServer.applyPlayground(app); // (Optional) Mounts the GraphQL Playground - do NOT use in Production
+app.use('/parse', parseServer.app); // (Optional) Mounts the REST API
+app.use('/dashboard', parseDashboard);
 // Endpoint for health check
 app.get('/health', function (req, res) {
   res.status(200).send('OK');
 });
 
-
-var port = process.env.PORT || 1337;
-var httpServer = require('http').createServer(app);
-httpServer.listen(port, function () {
-  console.log('Parse Server (and dashboard) started: ' + process.env.SERVER_URL + process.env.DASHBOARD_MOUNT);
+app.listen(process.env.PORT || 1337, function () {
+  console.log('REST API running on ' + process.env.SERVER_URL + '/parse');
+  console.log('Parse Dashboard running on ' + process.env.SERVER_URL + '/dashboard');
+  console.log('GraphQL API running on ' + process.env.SERVER_URL + '/graphql');
+  console.log('GraphQL Playground running on ' + process.env.SERVER_URL + '/playground');
 });
-
-// This will enable the Live Query real-time server
-ParseServer.createLiveQueryServer(httpServer);
