50b7934eb12f0c33bee8954648134a18549387a6
André Setti <ansetti7@gmail.com>
2020-07-24 16:09:10 -0300

    add inline env variables

--- index.js
+++ index.js
@@ -7,18 +7,22 @@ const fs = require('fs');
 
 dotenv.config();
 
+const DB_URI = process.env.DATABASE_URI || "mongodb://localhost:27017/refresco";
+const CLOUD_CODE = process.env.CLOUD_CODE_MAIN || "cloud/main.js";
+const APP_ID = process.env.APP_ID || "9UBUIZ0VeTdGe6YfwEg7KBbL8LSoM8ONAMQyLKzw";
+const MASTER_KEY = process.env.MASTER_KEY || "UIBXVLMy8cFCE1bqxUW0Bh5pPWC5FejtH4BDga1j";
+const SERVER_URL = process.env.SERVER_URL || "http://localhost:1337";
+
 const customSchema = fs.readFileSync('cloud/schema.graphql');
 
 const app = express();
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
-
 const parseServer = new ParseServer({
-  databaseURI: databaseUri,
-  cloud: process.env.CLOUD_CODE_MAIN,
-  appId: process.env.APP_ID,
-  masterKey: process.env.MASTER_KEY,
-  serverURL: process.env.SERVER_URL + '/parse',
+  databaseURI: DB_URI,
+  cloud: CLOUD_CODE,
+  appId: APP_ID,
+  masterKey: MASTER_KEY,
+  serverURL: SERVER_URL + '/parse',
 });
 
 const parseGraphQLServer = new ParseGraphQLServer(
@@ -26,18 +30,18 @@ const parseGraphQLServer = new ParseGraphQLServer(
   {
     graphQLPath: '/graphql',
     playgroundPath: '/playground',
-    graphQLCustomTypeDefs: gql`${customSchema}`
+    graphQLCustomTypeDefs: gql`${customSchema}`,
   }
 );
 
 const parseDashboard = new ParseDashboard({
   apps: [
     {
-      serverURL: process.env.SERVER_URL + '/parse',
-      appId: process.env.APP_ID,
-      masterKey: process.env.MASTER_KEY,
+      serverURL: SERVER_URL + '/parse',
+      appId: APP_ID,
+      masterKey: MASTER_KEY,
       appName: 'Refresco',
-      graphQLServerURL: process.env.SERVER_URL + '/graphql'
+      graphQLServerURL: SERVER_URL + '/graphql'
     }
   ]
 });
@@ -53,8 +57,8 @@ app.get('/health', function (req, res) {
 });
 
 app.listen(process.env.PORT || 1337, function () {
-  console.log('REST API running on ' + process.env.SERVER_URL + '/parse');
-  console.log('Parse Dashboard running on ' + process.env.SERVER_URL + '/dashboard');
-  console.log('GraphQL API running on ' + process.env.SERVER_URL + '/graphql');
-  console.log('GraphQL Playground running on ' + process.env.SERVER_URL + '/playground');
+  console.log('REST API running on ' + SERVER_URL + '/parse');
+  console.log('Parse Dashboard running on ' + SERVER_URL + '/dashboard');
+  console.log('GraphQL API running on ' + SERVER_URL + '/graphql');
+  console.log('GraphQL Playground running on ' + SERVER_URL + '/playground');
 });
